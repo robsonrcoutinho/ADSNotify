040229db27ea9edf57a33f2105bd8f0d00d703ab
Robson Coutinho <r.ramoscoutinho@gmail.com>
2016-05-20 12:06:46 -0300

    Leitura de PDF de calendário acadêmico - Parte 1

--- app/src/main/java/br/com/ifba/adsnotify/activity/DocumentoActivity.java
+++ app/src/main/java/br/com/ifba/adsnotify/activity/DocumentoActivity.java
@@ -4,21 +4,31 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.ifba.adsnotify.R;
+import br.com.ifba.adsnotify.adapters.RecyclerViewAdapter;
+import br.com.ifba.adsnotify.adapters.RecyclerViewAdapterDocumentos;
+import br.com.ifba.adsnotify.model.ItemObject;
 
 /**
  * Created by Robson on 19/05/2016.
  */
 public class DocumentoActivity extends AppCompatActivity {
+    private GridLayoutManager lLayout;
+
     @Override
     protected void onCreate(Bundle savedinstanceState) {
         super.onCreate(savedinstanceState);
         setContentView(R.layout.documento_layout);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarDocumento);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
 
@@ -28,6 +38,17 @@ public class DocumentoActivity extends AppCompatActivity {
             actionBar.setTitle("Documentos");
             actionBar.show();
         }
+
+
+        List<ItemObject> rowListItem = getAllItemList();
+        lLayout = new GridLayoutManager(getApplicationContext(), 2);
+
+        RecyclerView rView = (RecyclerView)findViewById(R.id.recycler_view_documento);
+        rView.setHasFixedSize(true);
+        rView.setLayoutManager(lLayout);
+
+        RecyclerViewAdapterDocumentos rcAdapter = new RecyclerViewAdapterDocumentos(getApplicationContext(), rowListItem);
+        rView.setAdapter(rcAdapter);
 
     }
 
@@ -41,4 +62,18 @@ public class DocumentoActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private List<ItemObject> getAllItemList() {
+
+        List<ItemObject> allItems = new ArrayList<>();
+        allItems.add(new ItemObject("Calendário Acadêmico", R.drawable.calendario));
+        allItems.add(new ItemObject("Ementa", R.drawable.ementa));
+        allItems.add(new ItemObject("Grade Curricular", R.drawable.grade));
+        allItems.add(new ItemObject("Horário Aulas ", R.drawable.horario));
+        allItems.add(new ItemObject("Normas Acadêmicas", R.drawable.normas));
+        allItems.add(new ItemObject("Projeto Pedagógico", R.drawable.projeto));
+
+
+        return allItems;
+
+    }
 }
--- app/src/main/java/br/com/ifba/adsnotify/adapters/RecyclerViewHoldersDoc.java
+++ app/src/main/java/br/com/ifba/adsnotify/adapters/RecyclerViewHoldersDoc.java
@@ -1,6 +1,5 @@
 package br.com.ifba.adsnotify.adapters;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
@@ -14,23 +13,23 @@ import br.com.ifba.adsnotify.activity.AvaliacaoActivity;
 import br.com.ifba.adsnotify.activity.DisciplinaActivity;
 import br.com.ifba.adsnotify.activity.DocumentoActivity;
 import br.com.ifba.adsnotify.activity.ProfessorActivity;
-import br.com.ifba.adsnotify.fragments.AvisoView;
+import br.com.ifba.adsnotify.docactivities.CalendarioAcademicoPDF;
 
 /**
  * Created by Robson on 24/04/2016.
  */
-public class RecyclerViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class RecyclerViewHoldersDoc extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     public TextView itemNome;
     public ImageView ItemImage;
     Context context;
 
 
-    public RecyclerViewHolders(View itemView) {
+    public RecyclerViewHoldersDoc(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
-        itemNome = (TextView)itemView.findViewById(R.id.item_name);
-        ItemImage = (ImageView)itemView.findViewById(R.id.item_photo);
+        itemNome = (TextView)itemView.findViewById(R.id.item_name_doc);
+        ItemImage = (ImageView)itemView.findViewById(R.id.item_photo_doc);
     }
 
     @Override
@@ -41,24 +40,21 @@ public class RecyclerViewHolders extends RecyclerView.ViewHolder implements View
 
         switch (position){
             case 0:
-                intent = new Intent(context,AvaliacaoActivity.class);
+                intent = new Intent(context,CalendarioAcademicoPDF.class);
                 context.startActivity(intent);
                 break;
             case 1:
-                Toast.makeText(view.getContext(), "Avisos = " + position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(view.getContext(), "Posicao = " + position, Toast.LENGTH_SHORT).show();
 
                 break;
             case 2:
-                intent = new Intent(context,DisciplinaActivity.class);
-                context.startActivity(intent);
+                Toast.makeText(view.getContext(), "Posicao = " + position, Toast.LENGTH_SHORT).show();
                 break;
             case 3:
-                intent = new Intent(context,DocumentoActivity.class);
-                context.startActivity(intent);
+                Toast.makeText(view.getContext(), "Posicao = " + position, Toast.LENGTH_SHORT).show();
                 break;
             case 4:
-                intent = new Intent(context,ProfessorActivity.class);
-                context.startActivity(intent);
+                Toast.makeText(view.getContext(), "Posicao = " + position, Toast.LENGTH_SHORT).show();
                 break;
         }
 
